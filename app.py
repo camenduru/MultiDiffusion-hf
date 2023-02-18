ecbe1997b732c2b9b0a98491204c05a64c9b95da
Omer Bar-Tal <omerbartal@users.noreply.huggingface.co>
2023-02-18 20:02:39 +0000

    Update app.py

--- app.py
+++ app.py
@@ -15,25 +15,25 @@ from diffusers import StableDiffusionPanoramaPipeline, DDIMScheduler
 
 model_ckpt = "stabilityai/stable-diffusion-2-base"
 scheduler = DDIMScheduler.from_pretrained(model_ckpt, subfolder="scheduler")
-# pipe = StableDiffusionPanoramaPipeline.from_pretrained(
-#      model_ckpt, scheduler=scheduler, torch_dtype=torch.float16
-# )
 pipe = StableDiffusionPanoramaPipeline.from_pretrained(
-     model_ckpt, scheduler=scheduler
+     model_ckpt, scheduler=scheduler, torch_dtype=torch.float16
 )
+# pipe = StableDiffusionPanoramaPipeline.from_pretrained(
+#      model_ckpt, scheduler=scheduler
+# )
 
 pipe = pipe.to(torch.device("cuda" if torch.cuda.is_available() else "cpu"))
 
-def generate_image_fn(prompt: str, guidance_scale: float) -> list:
+def generate_image_fn(prompt: str, img_width: int, img_height=512) -> list:
     start_time = time.time()
     prompt = "a photo of the dolomites"
-    image = pipe(prompt, guidance_scale=guidance_scale).images
+    image = pipe(prompt, height=img_height, width=img_width).images
     end_time = time.time()
     print(f"Time taken: {end_time - start_time} seconds.")
     return image
 
 
-description = "This Space demonstrates MultiDiffusion Text2Panorama using Stable Diffusion model. You can use it for generating custom pokemons. To get started, either enter a prompt and pick one from the examples below. For details on the fine-tuning procedure, refer to [this repository]()."
+description = "This Space demonstrates MultiDiffusion Text2Panorama using Stable Diffusion model. To get started, either enter a prompt and pick one from the examples below. For details, please visit [the project page](https://multidiffusion.github.io/)."
 article = "This Space leverages a T4 GPU to run the predictions. We use mixed-precision to speed up the inference latency."
 gr.Interface(
     generate_image_fn,
@@ -43,12 +43,12 @@ gr.Interface(
             max_lines=1,
             placeholder="a photo of the dolomites",
         ),
-        gr.Slider(value=40, minimum=8, maximum=50, step=1),
+        gr.Slider(value=4096, minimum=512, maximum=4608, step=128),
     ],
     outputs=gr.Gallery().style(grid=[2], height="auto"),
-    title="Generate custom pokemons",
+    title="Generate a panoramic image!",
     description=description,
     article=article,
-    examples=[["a photo of the dolomites", 40]],
+    examples=[["a photo of the dolomites", 4096]],
     allow_flagging=False,
 ).launch(enable_queue=True)
